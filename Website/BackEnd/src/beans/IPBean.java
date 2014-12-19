78a5acb3d9d7314daa48f777a9950db3f6edb3ea
willemwesterhof <www_willem@hotmail.com>
2014-12-19 13:36:02 +0100

    Create IPBean.java

---
+++ Website/BackEnd/src/beans/IPBean.java
@@ -0,0 +1,27 @@
+package beans;
+
+import java.util.ArrayList;
+
+/**
+ * Bean for getting all the user virtual machine information from database
+ * 
+ * @author willemWesterhof
+ *
+ */
+
+public class IPBean {
+	
+	/**
+	 * Setting the variables needed
+	 */
+	Private string IPAddress;
+
+	/**
+	 * Returning Available IP
+	 * 
+	 * @return
+	 */
+	public String getIPs() {
+		return IPAddress;
+	}
+}
