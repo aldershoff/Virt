ea0559091098434e30cfe41db88be46c7458331a
RDNY88 <RDNY@Rodneys-MacBook-Pro.local>
2015-01-12 11:45:13 +0100

    final versie

---
+++ Website/Admin Panel/Versie 0.4 Admin Panel_/js/chartData.js
@@ -0,0 +1,26 @@
+
+
+	var num1 = 200;
+	var num2 = 300;
+	
+
+	var barChartData = {
+		labels : ["Online","Offline"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1.8)",
+				highlightFill: "rgba(220,220,220,0.75)",
+				highlightStroke: "rgba(220,220,220,1)",
+				data : [num1, num2]
+			}
+			
+		]
+
+	}
+	window.onload = function(){
+		var ctx = document.getElementById("bchart").getContext("2d");
+		window.myBar = new Chart(ctx).Bar(barChartData, {
+			responsive : true
+		});
+	}
